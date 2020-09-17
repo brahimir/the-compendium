43758eaf4787909c2f2a05ec5a04a450edac8b4a
Nick <brahimir.nick@gmail.com>
2020-09-17 16:09:11 -0400

    Added starter demo layouts for project.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# TheCompendium
+# Metronic Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.7.
 
 ## Development server
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  isMockEnabled: true, // You have to switch this, when your real back-end is done
+  authTokenKey: 'authce9d77b308c149d5992a80073637e4d5'
 };
 
 /*
