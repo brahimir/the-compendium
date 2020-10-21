e04575a6e4f5488af7acd0b8ea513aaccde324ed
brahimir <brahimir.nick@gmail.com>
2020-10-21 13:53:12 -0400

    Adding Angular Material to pages module.

--- src/app/views/pages/pages.module.ts
+++ src/app/views/pages/pages.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 // Partials
 import { PartialsModule } from '../partials/partials.module';
+
 // Pages
 import { CoreModule } from '../../core/core.module';
 import { MailModule } from './apps/mail/mail.module';
@@ -17,10 +19,71 @@ import { MonstersAndNpcsComponent } from './the-compendium-pages/monsters-and-np
 import { SpellsComponent } from './the-compendium-pages/spells/spells.component';
 import { HomebrewComponent } from './the-compendium-pages/homebrew/homebrew.component';
 
+// Data table
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatSortModule } from '@angular/material/sort';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBottomSheetModule, MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+
 @NgModule({
   declarations: [MyPageComponent, WeaponsComponent, ItemsComponent, MonstersAndNpcsComponent, SpellsComponent, HomebrewComponent],
   exports: [],
   imports: [
+    // material modules
+		MatInputModule,
+		MatFormFieldModule,
+		MatAutocompleteModule,
+		MatListModule,
+		MatSliderModule,
+		MatCardModule,
+		MatSelectModule,
+		MatButtonModule,
+		MatMenuModule,
+		MatTabsModule,
+		MatTooltipModule,
+		MatSidenavModule,
+		MatProgressBarModule,
+		MatProgressSpinnerModule,
+		MatSnackBarModule,
+		MatTableModule,
+		MatGridListModule,
+		MatToolbarModule,
+		MatBottomSheetModule,
+		MatExpansionModule,
+		MatDividerModule,
+		MatSortModule,
+		MatStepperModule,
+		MatChipsModule,
+		MatPaginatorModule,
+		CoreModule,
+		MatTreeModule,
+		MatButtonToggleModule,
+		PartialsModule,
+		FormsModule,
     CommonModule,
     HttpClientModule,
     FormsModule,
