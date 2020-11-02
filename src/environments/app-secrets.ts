405cfb4e14c4c4bf431858e9f986fc02e91f9f3b
brahimir <brahimir.nick@gmail.com>
2020-11-02 11:49:36 -0500

    Added app-secrets in environments to house sensitive information.

---
+++ src/environments/app-secrets.ts
@@ -0,0 +1,9 @@
+// Mongo
+export const MONGO_ADMIN_PASSWORD = '99Zsefvd66';
+
+// API Routes
+export const routes = {
+    API_USERS_URL: 'api/users',
+    API_PERMISSION_URL: 'api/permissions',
+    API_ROLES_URL: 'api/roles'
+}
