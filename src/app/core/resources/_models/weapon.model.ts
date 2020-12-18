f2206368fe7ee1d61ec53e07d0f855eef0a24713
brahimir <brahimir.nick@gmail.com>
2020-12-18 15:10:19 -0500

    Added Weapon Object generation in HomebrewWeaponsService.

--- src/app/core/resources/_models/weapon.model.ts
+++ src/app/core/resources/_models/weapon.model.ts
@@ -10,7 +10,23 @@ export class Weapon {
     value: string;
     properties: string;
     description: string;
+    ratings: number[];
 
+    /**
+     * 
+     * @param id The Weapon id
+     * @param name The Weapon name
+     * @param type The Weapon type
+     * @param is_martial True if the Weapon is a martial Weapon, False otherwise
+     * @param damage The Weapon damage
+     * @param damage_type The Weapon damamge type
+     * @param requires_attunement True if the Weapon requires attunement, False otherwise
+     * @param rarity The Weapon rarity (Common, Uncommon, Rare, Very Rare)
+     * @param value The Weapon value
+     * @param properties The Weapon properties
+     * @param description The Weapon description
+     * @param ratings The Weapon ratings
+     */
     constructor(
         id: number,
         name: string,
@@ -23,17 +39,19 @@ export class Weapon {
         value: string,
         properties: string,
         description: string,
+        ratings: number[]
     ) {
         this.id = id;
         this.name = name;
-        this.type = type;
-        this.is_martial = is_martial;
-        this.damage = damage;
-        this.damage_type = damage_type;
-        this.requires_attunement = requires_attunement;
-        this.rarity = rarity;
-        this.value = value;
-        this.properties = properties;
-        this.description = description;
+        this.type = type || null;
+        this.is_martial = is_martial || null;
+        this.damage = damage || null;
+        this.damage_type = damage_type || null;
+        this.requires_attunement = requires_attunement || null;
+        this.rarity = rarity || null;
+        this.value = value || null;
+        this.properties = properties || null;
+        this.description = description || null;
+        this.ratings = ratings || null;
     }
 }
--- src/app/core/resources/_services/homebrew-services/homebrew-weapons.service.ts
+++ src/app/core/resources/_services/homebrew-services/homebrew-weapons.service.ts
@@ -1,48 +1,88 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 // Models
-import { Weapon } from '../../_models/weapon.model';
+import { Weapon } from "../../_models/weapon.model";
 // Routes
 import { routes } from "../../../../../environments/app-secrets";
 // Http
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
 // RXJS
-import { Observable } from 'rxjs';
+import { Observable, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
-const URL = routes.HOMEBREW.WEAPONS;
+const BASE_URL = routes.HOMEBREW.WEAPONS;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-
 export class HomebrewWeaponsService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get(URL)
+    return this.http.get(BASE_URL).pipe(
+      map((data) => {
+        let weaponObjects = this.generateArmorObjects(data);
+        return weaponObjects;
+      }),
+
+      catchError((error) => {
+        console.error(error);
+        return throwError("Something went wrong accessing the API.");
+      })
+    );
   }
 
   get(id): Observable<any> {
-    return this.http.get(`${URL}/${id}`);
+    return this.http.get(`${BASE_URL}/${id}`);
   }
 
   create(data): Observable<any> {
-    return this.http.post(URL, data);
+    return this.http.post(BASE_URL, data);
   }
 
   update(id, data): Observable<any> {
-    return this.http.put(`${URL}/${id}`, data);
+    return this.http.put(`${BASE_URL}/${id}`, data);
   }
 
   delete(id): Observable<any> {
-    return this.http.delete(`${URL}/${id}`);
+    return this.http.delete(`${BASE_URL}/${id}`);
   }
 
   deleteAll(): Observable<any> {
-    return this.http.delete(URL);
+    return this.http.delete(BASE_URL);
   }
 
   findByTitle(title): Observable<any> {
-    return this.http.get(`${URL}?title=${title}`);
+    return this.http.get(`${BASE_URL}?title=${title}`);
+  }
+
+  /**
+   * Returns an array of Weapon Objects (defined by the model).
+   * 
+   * @param data Raw data from API.
+   * @returns {Weapon[]} The array of Weapon Objects.
+   */
+  generateArmorObjects(data: any): Weapon[] {
+    let armorObjects: Weapon[] = [];
+
+    data.forEach((element) => {
+      armorObjects.push(
+        new Weapon(
+          element._id,
+          element.name,
+          element.type,
+          element.is_martial,
+          element.damage,
+          element.damage_type,
+          element.requires_attunement,
+          element.rarity,
+          element.value,
+          element.properties,
+          element.description,
+          element.ratings
+        )
+      );
+    });
+
+    return armorObjects;
   }
 }
