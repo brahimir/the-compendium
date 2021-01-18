83633dfda6a4f9e53efd4878479d74724fa5cdc0
brahimir <brahimir.nick@gmail.com>
2021-01-18 15:46:49 -0500

    Removing "Adventures" - renaming to Session Summaries. Cleaning up unused directories.

--- src/app/views/pages/the-compendium-pages/_dm-tools/storyboard/constants.ts
+++
@@ -1,8 +0,0 @@
-export const CONSTANTS_STORYBOARD = {
-  REMOVE_PLOT: {
-    CONFIRMATION_MESSAGE:
-      "Are you sure you want to remove this plot?",
-    WARNING_MESSAGE: "Warning - this action cannot be undone."
-  },
-  PLOT_OPTIONS: ["Edit", "Remove"],
-};
