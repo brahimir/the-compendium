3c2cf3b0a068eba4bdc6ae33b4b7122162a84d5b
brahimir <brahimir.nick@gmail.com>
2021-01-18 15:44:33 -0500

    Adding CreateSessionComponent.

---
+++ src/app/views/pages/the-compendium-pages/_dialogs/create-dialog/create-session-dialog/create-session-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kt-create-session-dialog',
+  templateUrl: './create-session-dialog.component.html',
+  styleUrls: ['./create-session-dialog.component.scss']
+})
+export class CreateSessionDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
