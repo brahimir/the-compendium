6a54ef707b3b76a2958b2be21aa3f1913b18d32c
brahimir <brahimir.nick@gmail.com>
2021-01-18 15:48:17 -0500

    Adding SessionSummariesService.

---
+++ src/app/core/resources/_services/dm-tools-services/session_summaries.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from "@angular/core";
+// Models
+import { Session } from "src/app/core/resources/_models/dm_tools/session_summaries/session.model";
+// Routes
+import { API_ROUTES } from "../../../../../environments/app-secrets";
+// Http
+import { HttpClient } from "@angular/common/http";
+// RXJS
+import { Observable, throwError } from "rxjs";
+import { map, catchError } from "rxjs/operators";
+
+// API urls
+const USERS_URL = API_ROUTES.AUTH.USERS;
+const UPDATE_SESSIONS = API_ROUTES.DM_TOOLS.UPDATE_SESSIONS;
+
+@Injectable({
+  providedIn: "root",
+})
+export class SessionSummariesService {
+  constructor(private http: HttpClient) {}
+
+  getSessionSummaries(userId: string): Observable<any> {
+    return this.http.get(`${USERS_URL}${userId}`);
+  }
+
+  updateSessionSummaries(userId: string, body: any): Observable<any> {
+    return this.http.put(`${UPDATE_SESSIONS}${userId}`, body);
+  }
+
+  /**
+   * Returns an array of Plot Objects (defined by the model).
+   *
+   * @param data Raw data from API.
+   * @returns {Session[]} The array of Plot Objects.
+   */
+  generateSessionObjects(data: any): Session[] {
+    let objects: Session[] = [];
+
+    data.forEach((element) => {
+      objects.push(
+        new Session(
+          element.titleChapter,
+          element.titleEpisode,
+          element.date,
+          element.content
+        )
+      );
+    });
+
+    return objects;
+  }
+}
