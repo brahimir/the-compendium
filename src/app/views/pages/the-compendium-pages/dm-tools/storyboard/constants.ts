d1942862b478f142d42626377d91995f2310dc6c
brahimir <brahimir.nick@gmail.com>
2021-01-18 15:47:06 -0500

    Added Storyboard contants.

---
+++ src/app/views/pages/the-compendium-pages/dm-tools/storyboard/constants.ts
@@ -0,0 +1,8 @@
+export const CONSTANTS_STORYBOARD = {
+  REMOVE_PLOT: {
+    CONFIRMATION_MESSAGE:
+      "Are you sure you want to remove this plot?",
+    WARNING_MESSAGE: "Warning - this action cannot be undone."
+  },
+  PLOT_OPTIONS: ["Edit", "Remove"],
+};
