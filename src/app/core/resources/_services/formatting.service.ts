4926ba53f6da55e61bafd7138368238e3d2f5f3f
brahimir <brahimir.nick@gmail.com>
2021-01-22 13:38:46 -0500

    Refactoring TypesUtils and FormattingService.

--- src/app/core/resources/_services/formatting.service.ts
+++ src/app/core/resources/_services/formatting.service.ts
@@ -7,7 +7,7 @@ export class FormattingService {
   constructor() {}
 
   /**
-   * Replaces spaces with underscores from on incoming array
+   * Replaces spaces with underscores from incoming array
    *
    * @param {string[]} data The array to process
    * @returns {string[]} The proccessed array
@@ -22,12 +22,82 @@ export class FormattingService {
   }
 
   /**
-   * Replaces spaces with underscores from on string
+   * Replaces spaces with underscores from string
    *
    * @param {string} data The string to process
    * @returns {string} The proccessed string
    */
   replaceUnderscoresWithSpaces(data: string): string {
     return data.replace("_", " ");
+  }
+
+  /**
+   * Lowercases first letter in a strng.
+   *
+   * @param {*} string The string to format.
+   * @returns The formatted string.
+   */
+  lowercaseFirstLetter(data: string) {
+    return data.charAt(0).toLowerCase() + data.slice(1);
+  }
+
+  /**
+   * Capitalizes first letter in a strng.
+   *
+   * @param {*} string The string to format.
+   * @returns The formatted string.
+   */
+  capitalizeFirstLetter(data: string) {
+    return data.charAt(0).toUpperCase() + data.slice(1);
+  }
+
+  /**
+   * Lowercases each word in a string.
+   *
+   * @param {string} data The string to format.
+   * @returns The fotmatted string.
+   */
+  lowercaseAllLetters(data: string) {
+    let splitStr = data.toLowerCase().split(" ");
+
+    for (let i = 0; i < splitStr.length; i++) {
+      splitStr[i] =
+        splitStr[i].charAt(0).toLowerCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(" ");
+  }
+
+  /**
+   * Uppercases each letter in a string.
+   *
+   * @param {string} data The string to format.
+   * @returns The fotmatted string.
+   */
+  uppercaseAllLetters(data: string) {
+    let splitStr = data.toUpperCase().split(" ");
+
+    for (let i = 0; i < splitStr.length; i++) {
+      splitStr[i] =
+        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    return splitStr.join(" ");
+  }
+
+  /**
+   * Titlecases every word in a string.
+   *
+   * @param {string} data The string to format.
+   * @returns The formatted string.
+   */
+  titleCase(data: string) {
+    var splitStr = data.toLowerCase().split(" ");
+    for (var i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] =
+        splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(" ");
   }
 }
