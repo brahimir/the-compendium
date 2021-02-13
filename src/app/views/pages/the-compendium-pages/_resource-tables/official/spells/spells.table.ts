3c311ea01508426548b491d6e44ded347a284b18
brahimir <brahimir.nick@gmail.com>
2021-02-13 16:24:31 -0500

    Added missing Spells.

--- src/app/views/pages/the-compendium-pages/_resource-tables/official/spells/spells.table.ts
+++ src/app/views/pages/the-compendium-pages/_resource-tables/official/spells/spells.table.ts
@@ -9922,5 +9922,120 @@ export const spells: Spell[] = [
       { index: "ranger", name: "Ranger", url: "/api/classes/ranger" }
     ],
     subclasses: [{ index: "lore", name: "Lore", url: "/api/subclasses/lore" }]
+  },
+  {
+    name: "Wish",
+    desc: [
+      "Wish is the mightiest spell a mortal creature can cast. By simply speaking aloud, you can alter the very foundations of reality in accord with your desires.",
+      "The basic use of this spell is to duplicate any other spell of 8th level or lower. You don't need to meet any requirements in that spell, including costly components. The spell simply takes effect.",
+      "Alternatively, you can create one of the following effects of your choice:",
+      "- You create one object of up to 25,000 gp in value that isn't a magic item. The object can be no more than 300 feet in any dimension, and it appears in an unoccupied space you can see on the ground.",
+      "- You allow up to twenty creatures that you can see to regain all hit points, and you end all effects on them described in the greater restoration spell.",
+      "- You grant up to ten creatures that you can see resistance to a damage type you choose.",
+      "- You grant up to ten creatures you can see immunity to a single spell or other magical effect for 8 hours. For instance, you could make yourself and all your companions immune to a lich's life drain attack.",
+      "- You undo a single recent event by forcing a reroll of any roll made within the last round (including your last turn). Reality reshapes itself to accommodate the new result. For example, a wish spell could undo an opponent's successful save, a foe's critical hit, or a friend's failed save. You can force the reroll to be made with advantage or disadvantage, and you can choose whether to use the reroll or the original roll.",
+      "You might be able to achieve something beyond the scope of the above examples. State your wish to the GM as precisely as possible. The GM has great latitude in ruling what occurs in such an instance; the greater the wish, the greater the likelihood that something goes wrong. This spell might simply fail, the effect you desire might only be partly achieved, or you might suffer some unforeseen consequence as a result of how you worded the wish. For example, wishing that a villain were dead might propel you forward in time to a period when that villain is no longer alive, effectively removing you from the game. Similarly, wishing for a legendary magic item or artifact might instantly transport you to the presence of the item's current owner.",
+      "The stress of casting this spell to produce any effect other than duplicating another spell weakens you. After enduring that stress, each time you cast a spell until you finish a long rest, you take 1d10 necrotic damage per level of that spell. This damage can't be reduced or prevented in any way. In addition, your Strength drops to 3, if it isn't 3 or lower already, for 2d4 days. For each of those days that you spend resting and doing nothing more than light activity, your remaining recovery time decreases by 2 days. Finally, there is a 33 percent chance that you are unable to cast wish ever again if you suffer this stress."
+    ],
+    range: "Self",
+    components: ["V"],
+    ritual: false,
+    duration: "Instantaneous",
+    concentration: false,
+    casting_time: "1 action",
+    level: 9,
+    school: {
+      index: "conjuration",
+      name: "Conjuration",
+      url: "/api/magic-schools/conjuration"
+    },
+    classes: [
+      {
+        index: "sorcerer",
+        name: "Sorcerer",
+        url: "/api/classes/sorcerer"
+      },
+      {
+        index: "wizard",
+        name: "Wizard",
+        url: "/api/classes/wizard"
+      }
+    ],
+    subclasses: []
+  },
+  {
+    name: "Word of Recall",
+    desc: [
+      "You and up to five willing creatures within 5 feet of you instantly teleport to a previously designated sanctuary. You and any creatures that teleport with you appear in the nearest unoccupied space to the spot you designated when you prepared your sanctuary (see below). If you cast this spell without first preparing a sanctuary, the spell has no effect.",
+      "You must designate a sanctuary by casting this spell within a location, such as a temple, dedicated to or strongly linked to your deity. If you attempt to cast the spell in this manner in an area that isn't dedicated to your deity, the spell has no effect."
+    ],
+    range: "5 feet",
+    components: ["V"],
+    ritual: false,
+    duration: "Instantaneous",
+    concentration: false,
+    casting_time: "1 action",
+    level: 6,
+    school: {
+      index: "conjuration",
+      name: "Conjuration",
+      url: "/api/magic-schools/conjuration"
+    },
+    classes: [
+      {
+        index: "cleric",
+        name: "Cleric",
+        url: "/api/classes/cleric"
+      }
+    ],
+    subclasses: []
+  },
+  {
+    name: "Zone of Truth",
+    desc: [
+      "You create a magical zone that guards against deception in a 15-foot-radius sphere centered on a point of your choice within range. Until the spell ends, a creature that enters the spell's area for the first time on a turn or starts its turn there must make a Charisma saving throw. On a failed save, a creature can't speak a deliberate lie while in the radius. You know whether each creature succeeds or fails on its saving throw.",
+      "An affected creature is aware of the fate and can avoid answering questions she would normally have responded with a lie. Such a creature can remain evasive in his answers as they remain within the limits of truth."
+    ],
+    range: "60 feet",
+    components: ["V", "S"],
+    ritual: false,
+    duration: "10 minutes",
+    concentration: false,
+    casting_time: "1 action",
+    level: 2,
+    school: {
+      index: "enchantment",
+      name: "Enchantment",
+      url: "/api/magic-schools/enchantment"
+    },
+    classes: [
+      {
+        index: "bard",
+        name: "Bard",
+        url: "/api/classes/bard"
+      },
+      {
+        index: "cleric",
+        name: "Cleric",
+        url: "/api/classes/cleric"
+      },
+      {
+        index: "paladin",
+        name: "Paladin",
+        url: "/api/classes/paladin"
+      }
+    ],
+    subclasses: [
+      {
+        index: "lore",
+        name: "Lore",
+        url: "/api/subclasses/lore"
+      },
+      {
+        index: "devotion",
+        name: "Devotion",
+        url: "/api/subclasses/devotion"
+      }
+    ]
   }
 ];
